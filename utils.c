ce6b2cb33edf1083923a9a85c7d95b5a79ca8920
wiamaiz <ouiame.faiz@gmail.com>
2023-04-25 18:03:38 -0700

    addded codes

--- utils.c
+++ utils.c
@@ -1,105 +1,84 @@
 #include "main.h"
 
 /**
- * _strlen - length of a given string
- * @s: the string
- * Return: the length of given string
- */
-
-unsigned int _strlen(char *s)
+   * is_printable - Evaluates if a char is printable
+    * @c: Char to be evaluated.
+     *
+      * Return: 1 if c is printable, 0 otherwise
+       */
+int is_printable(char c)
 {
-	unsigned int len = 0;
+	if (c >= 32 && c < 127)
+		return (1);
 
-	while (s[len])
-		len++;
-
-	return (len);
+	return (0);
 }
 
 /**
- * reverse_str - reverses a given string
- * @s: the string to be reversed
- */
-
-void reverse_str(char s[])
+   * append_hexa_code - Append ascci in hexadecimal code to buffer
+    * @buffer: Array of chars.
+     * @i: Index at which to start appending.
+      * @ascii_code: ASSCI CODE.
+       * Return: Always 3
+	*/
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
-	unsigned int i = 0, len = 0;
-	char temp;
+	char map_to[] = "0123456789ABCDEF";
 
-	len = _strlen(s);
+	if (ascii_code < 0)
+		ascii_code *= -1;
 
-	for (; i < len / 2; i++)
-	{
-		temp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
-	}
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+	return (3);
 }
 
 /**
- * _itoa - Converts a long integer to a string
- *  @n: The long integer to convert
- *  @s: The character array to store the converted string in
- */
-
-void _itoa(long n, char s[])
-{
-	long sign = n;
-	unsigned int i = 0;
-
-	if (sign < 0)
-		n = -n;
-	do {
-		s[i++] = n % 10 + '0';
-	} while ((n /= 10) > 0);
-	if (sign < 0)
-		s[i++] = '-';
-	s[i] = '\0';
-	reverse_str(s);
-}
-
-/**
- * to_base_n - convert a number to a base n
- * @num: the number to convert
- * @base: the base to convert to
- * @buffer: the character array that will hold the resulting string
- * Return: number of characters printed
- */
-
-int to_base_n(unsigned long num, int base, char buffer[])
-{
-	int i = 0, rem;
-	char hex_case = (buffer[0] == 'A') ? 'A' : 'a';
-
-	(num == 0) && (buffer[i++] = '0');
-
-	while (num > 0)
-	{
-		rem = num % base;
-
-		if (rem < 10)
-			buffer[i++] = rem + '0';
-		else
-			buffer[i++] = rem - 10 + hex_case;
-
-		num /= base;
-	}
-	buffer[i] = '\0';
-	reverse_str(buffer);
-	return (_puts(buffer));
-}
-
-/**
- * _isdigit - checks for a digit
- * @c: the character to be checked
- *
- * Return: 1 if c is a digit, else 0
- */
-
-int _isdigit(int c)
+   * is_digit - Verifies if a char is a digit
+    * @c: Char to be evaluated
+     *
+      * Return: 1 if c is a digit, 0 otherwise
+	*/
+int is_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-	else
-		return (0);
+
+	return (0);
+}
+
+/**
+   * convert_size_number - Casts a number to the specified size
+    * @num: Number to be casted.
+     * @size: Number indicating the type to be casted.
+      *
+       * Return: Casted value of num
+	*/
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+   * convert_size_unsgnd - Casts a number to the specified size
+    * @num: Number to be casted
+     * @size: Number indicating the type to be casted
+      *
+       * Return: Casted value of num
+	*/
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
 }
